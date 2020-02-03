15d94f5ba353befc5ab8a8a56f393b0150510f94
Karen Daniela <kdaniimora@gmail.com>
2020-02-03 22:28:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
 
 holiday_hash.each do |season, holidays|
  
-  puts "#{season.capitalize!}"
+  puts "#{season.capitalize}"
   holidays.each do |holiday, supply|
     puts "#{holiday.capitalize}"
     supply.each do |supply|
